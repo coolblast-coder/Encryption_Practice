750d6ebb079481371a40835fda626b180a6eafcf
Coolblast-coder <lyapunovantik@gmail.com>
2023-07-17 13:40:55 -0600

    Implemented bcrypt and salting rounds

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption")
+const md5 = require("md5");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
 
 const app = express();
 
@@ -22,7 +25,6 @@ const userSchema = new mongoose.Schema ({
 });
 
 
-userSchema.plugin(encrypt, {secret:process.env.SECRET, encryptedFields:["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -35,15 +37,19 @@ app.get("/", function (req,res) {
  })
 
 app.post("/register", function (req,res) { 
-    const newUser = new User({
-        email: req.body.username,
-        password: req.body.password
-    })
-    newUser.save().then(function () { 
-        res.render("secrets");
-     }).catch(function(){
-        console.log(err);
-     })
+
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            password: hash
+        })
+        newUser.save().then(function () { 
+            res.render("secrets");
+         }).catch(function(){
+            console.log(err);
+         })
+    });
+
  })
 
 
@@ -58,9 +64,13 @@ app.post("/login", function (req,res) {
 
     User.findOne({email: username}).then(function (foundUser) { 
         if(foundUser){
-            if(foundUser.password === password){
-                res.render("secrets");
-            }
+            bcrypt.compare(password, foundUser.password, function(err, result) {
+                if(result === true){
+                    res.render("secrets");
+                }
+            });
+                
+            
         }
      }).catch(function(err){
         console.log(err);
